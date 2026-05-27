e961c6f8b1af90ec1c8b1ed3d9196bf601cb19f5
Patrick Morpey <patrick@morphp.nl>
2026-05-27 11:03:50 +0200

    Fix `TableStructureFinder` regex to remove space before parentheses

--- src/TableStructureFinder.php
+++ src/TableStructureFinder.php
@@ -115,7 +115,7 @@ readonly class TableStructureFinder
 
         return array_map(function (string $name): string {
             // Strip backticks and any trailing prefix length e.g. (500)
-            $name = preg_replace('/ \(\d+\)$/', '', $name);
+            $name = preg_replace('/\(\d+\)$/', '', $name);
 
             return trim($name, '`');
         }, $names);
